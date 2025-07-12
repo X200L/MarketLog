bdc1eee2de5b20222cb3dbf314b5b8c3714859e0
Stark1113r <dfcmn@yandex.com>
2025-07-12 11:08:54 +0300

    Finished single agent pathfinder

---
+++ visualization/main/usr_lib/singleagentplanner.py
@@ -0,0 +1,43 @@
+import json
+from pathlib import Path
+import dijkstra
+import test
+#Инициализация
+def drawagentmap(target, agentsloc, pathmap, start):
+    #pathmap - 2х-мерный массив с картой из двух типов знаков (стена - 1, стеной считаются все клетки, через которые не может проехать робот со стеллажом, и дорога - 0)
+    #agentsloc - локации всех агентов кроме текущего в виде 2х-мерного массива
+    #target - цель агента
+    #start - текущая локация агента
+    graph = {}
+    if agentsloc:
+        for i in range(0, len(agentsloc)):
+            pathmap[agentsloc[i][0]][agentsloc[i][1]] = 1
+    pathmap[target[0]][target[1]] = 0
+    print(len(pathmap[10]))
+    for i in range(0, len(pathmap)):
+        #print(i)
+        for j in range(0, len(pathmap[i])):
+            #print(j)
+            graph[(str(i)+':'+str(j))] = []
+            if pathmap[i][j] == '0':
+                #print('loc: '+str(pathmap[i][j]))
+                if pathmap[i][j] == '17:29':
+                    print('test')
+                if pathmap[i+1][j] == '0':
+                    graph[(str(i)+':'+str(j))].append((str(i+1)+':'+str(j)))
+                    #print('connection down')
+                if pathmap[i][j+1] == '0':
+                    graph[(str(i)+':'+str(j))].append((str(i)+':'+str(j+1)))
+                    #print('connection right')
+                if pathmap[i-1][j] == '0':
+                    graph[(str(i)+':'+str(j))].append((str(i-1)+':'+str(j)))
+                    #print('connection up')
+                if pathmap[i][j-1] == '0':
+                    graph[(str(i)+':'+str(j))].append((str(i)+':'+str(j-1)))
+                    #print('connection left')
+    print(graph)
+    print(dijkstra.dijkstra(graph,'5:8','17:28'))
+t = [17,29]
+a = []
+l = [5,8]
+drawagentmap(t,a,test.readtxtmap(),l)
