627bb1289dffc3990c6ea31f246f7da37d81b324
Stark1113r <dfcmn@yandex.com>
2025-07-16 14:27:15 +0300

    Implemented collision type 1 avoidance
    aga

--- visualization/main/usr_lib/singleagentplanner.py
+++ visualization/main/usr_lib/singleagentplanner.py
@@ -3,15 +3,12 @@ from pathlib import Path
 import usr_lib.dijkstra as dijkstra
 import usr_lib.readmaptxt
 #Инициализация
-def drawagentmap(target, agentsloc, pathmap, start):
+def drawagentmap(target, agentspaths, pathmap, start, f):
     #pathmap - 2х-мерный массив с картой из двух типов знаков (стена - 1, стеной считаются все клетки, через которые не может проехать робот со стеллажом, и дорога - 0)
     #agentsloc - локации всех агентов кроме текущего в виде 2х-мерного массива
     #target - цель агента
     #start - текущая локация агента
     graph = {}
-    if agentsloc:
-        for i in range(0, len(agentsloc)):
-            pathmap[agentsloc[i][0]][agentsloc[i][1]] = '1'
     pathmap[target[0]][target[1]] = '0'
     #print(len(pathmap[10]))
     for i in range(0, len(pathmap)):
@@ -36,4 +33,4 @@ def drawagentmap(target, agentsloc, pathmap, start):
                     graph[(str(i)+':'+str(j))].append((str(i)+':'+str(j-1)))
                     #print('connection left')
     #print(graph)
-    return dijkstra.dijkstra(graph,str(start[0])+':'+str(start[1]),str(target[0])+':'+str(target[1]))
+    return dijkstra.dijkstra(graph,str(start[0])+':'+str(start[1]),str(target[0])+':'+str(target[1]), agentspaths, f)
