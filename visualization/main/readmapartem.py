622a417365237f3cc3b774f948885c20813aee07
Stark1113r <dfcmn@yandex.com>
2025-07-23 09:42:43 +0300

    finalfinalfinalshit

--- visualization/main/readmapartem.py
+++ visualization/main/readmapartem.py
@@ -2,20 +2,29 @@ from pathlib import Path
 import usr_lib.objects as obj
 shelves = []
 start = []
-def readtxtmap(path):
+def readtxtmap():
     pathmap = []
-    with open(Path(path), "r", encoding="utf-8") as file:
+    with open(Path("../main/usr_lib/map.txt"), "r", encoding="utf-8") as file:
         mapl = file.readlines()  # Возвращает список строк
     for i in range(0,len(mapl)):
         pathmap.append([])
         pathmap[i] = mapl[i].split()
         a = ''
         for j in range(0,len(pathmap[i])):
+            if pathmap[i][j]=="2" or pathmap[i][j]=="4":
+                pathmap[i][j]='0'
+            elif pathmap[i][j]=="1"or pathmap[i][j]=="3":
+                pathmap[i][j]='2'
+            elif pathmap[i][j]=="-1":
+                pathmap[i][j]='%'
+            elif pathmap[i][j]=="-3" or pathmap[i][j]=="-2":
+                pathmap[i][j]='1'
             a = a + ' '+pathmap[i][j]
             if pathmap[i][j] == '2':
                 shelves.append(obj.shelf(i,j))
             if pathmap[i][j] == '%':
                 start = [i, j]
         print(a)
+    
     #print(shelves)
     return pathmap, start
--- visualization/main/usr_lib/singleagentplanner.py
+++ visualization/main/usr_lib/singleagentplanner.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 import usr_lib.dijkstrav2 as dijkstra
-import usr_lib.readmaptxt
+import usr_lib.readmaptxt as maptxt
 #Инициализация
 def drawagentmap(target, agentspaths, pathmap, start, f):
     #pathmap - 2х-мерный массив с картой из двух типов знаков (стена - 1, стеной считаются все клетки, через которые не может проехать робот со стеллажом, и дорога - 0)
@@ -14,11 +14,10 @@ def drawagentmap(target, agentspaths, pathmap, start, f):
     #print(len(pathmap[10]))
     d = 0
     for i in range(0, len(pathmap)):
-        #print(i)
         for j in range(0, len(pathmap[i])):
             #print(j)
             graph[(str(i)+':'+str(j))] = []
-            graph[str(f)+str(d)] = []
+            #graph[str(f)+str(d)] = []
             if pathmap[i][j] == '0' or pathmap[i][j]=='R' or pathmap[i][j]=='2'  or pathmap[i][j]=='S'  or pathmap[i][j]=='%':
                 if pathmap[i+1][j] == '0' or pathmap[i+1][j] == 'R' or pathmap[i+1][j] == 'S' or pathmap[i+1][j] == '%':
                     graph[(str(i)+':'+str(j))].append((str(i+1)+':'+str(j)))
@@ -28,30 +27,58 @@ def drawagentmap(target, agentspaths, pathmap, start, f):
                     #print('connection right')
                 if pathmap[i-1][j] == '0' or pathmap[i-1][j] == 'R'or pathmap[i-1][j] == 'S'or pathmap[i-1][j] == '%':
                     graph[(str(i)+':'+str(j))].append((str(i-1)+':'+str(j)))
-                    graph[(str(i)+':'+str(j))].append(str(f)+str(d))
-                    graph[str(f)+str(d)].append((str(i)+':'+str(j)))
+                    #graph[(str(i)+':'+str(j))].append(str(f)+str(d))
+                    #graph[str(f)+str(d)].append((str(i)+':'+str(j)))
                     #print('connection up')
                 if pathmap[i][j-1] == '0' or pathmap[i][j-1] == 'R' or pathmap[i][j-1] == 'S' or pathmap[i][j-1] == '%':
                     graph[(str(i)+':'+str(j))].append((str(i)+':'+str(j-1)))
-                graph[(str(i)+':'+str(j))].append(str(f)+str(d))
-                graph[str(f)+str(d)].append((str(i)+':'+str(j)))
+                #graph[(str(i)+':'+str(j))].append(str(f)+str(d))
+                #graph[str(f)+str(d)].append((str(i)+':'+str(j)))
                     #print('connection left')
                 d+=1
+    #print(graph[str(target[0])+':'+str(target[1])])
+    #print('----')
     dist, paths  = dijkstra.dijkstra(start, graph)
-    #print(paths)
-    #print(dist)
     current = str(target[0])+':'+str(target[1])
-    j = 0
-    while True:
-        if current == str(start[0])+':'+str(start[1]):
-            break
-        path.append(paths[current])
-        current = paths[current]
-    for node in path:
-        path.append(path[0])
-        path.pop(0)
+    path.append(current)
+    t=1
+    x=0
+    while path[-1]!=str(start[0])+':'+str(start[1]):
+        currays = paths[current]
+        #print(currays)
+        for l in range(len(agentspaths)):
+            #print('stage1')
+            if t<=len(agentspaths[l]):
+                #print('stage2')
+                if agentspaths[l]:
+                    for s in range(len(currays)):
+                        #print(str(currays[0])+'?'+str(agentspaths[l][-t]))
+                        if agentspaths[l][-t]==currays[0] or currays[0] in path:
+                            print(path)
+                            print(str(currays[0])+'='+str(agentspaths[l][-t]))
+                            currays.pop(0)
+
+        if not(currays):
+            return
+        if x<len(currays):
+        
+            if not(currays[x] in path):
+                path.append(currays[x])
+                current = currays[x]
+                x = 0
+            else:
+                x+=1
+        else:
+            return
+        t+=1
+
+    #print('---')
     #print(path)
+    #print('---')
     return path
+
+
+
 def startcords(pathmap):
     graph = {}
     w = []
